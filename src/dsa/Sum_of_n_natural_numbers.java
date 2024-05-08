f5f50fa2ed492db1916f1f813a7dfe7af4c30237
CHANDANJSLC <chandanjslc2001@gmail.com>
2024-05-08 14:20:38 +0530

    First commit

---
+++ src/dsa/Sum_of_n_natural_numbers.java
@@ -0,0 +1,13 @@
+package dsa;
+import java.util.*;
+public class Sum_of_n_natural_numbers {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n= scan.nextInt();
+		int sum=(n*(n+1))/2;//Logic for sum of n natural numbers.
+		
+		System.out.println(sum);
+	}
+}
