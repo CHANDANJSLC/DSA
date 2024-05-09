51dbf46bf23ebb4b0c7fc19d9787e0315fac5797
CHANDANJSLC <chandanjslc2001@gmail.com>
2024-05-09 18:39:47 +0530

    8th program

---
+++ src/dsa/Prime_number_8.java
@@ -0,0 +1,33 @@
+package dsa;
+
+import java.util.*;
+public class Prime_number_8 {
+	
+	static void isPrime(int n) {
+		if(n==0 || n==1) {
+			System.out.println("Not a prime number");
+			return;
+			}
+			
+			for(int i=2;i<=n/2;i++) 
+			{
+				if(n%i==0) 
+				{
+					System.out.println("It is not a prime number");
+					return;
+				} 	
+			}
+			System.out.println("It is a prime number");
+	}
+	
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		int n=scan.nextInt();
+		
+		isPrime(n);	
+
+			scan.close();
+		}
+		
+
+}
