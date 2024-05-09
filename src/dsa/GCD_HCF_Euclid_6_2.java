5bdcd901c9b8530ad55599e2f2dc9d19accee9a8
CHANDANJSLC <chandanjslc2001@gmail.com>
2024-05-09 10:55:31 +0530

    6th program added by euclid algorithm

---
+++ src/dsa/GCD_HCF_Euclid_6_2.java
@@ -0,0 +1,23 @@
+package dsa;
+
+import java.util.Scanner;
+
+public class GCD_HCF_Euclid_6_2 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt(); 
+		int b= scan.nextInt();
+		
+		while(a!=b) { //If both the numbers a and b are same then come out of loop.
+			if(a>b) { //Larger number 
+				a=a-b; //Substract and replace with larger number.
+			} else {
+				b=b-a;
+			}
+		}
+		System.out.println(a); //print any of the one a or b.
+	}
+
+}
