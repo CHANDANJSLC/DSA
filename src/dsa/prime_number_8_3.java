bb651a89a47fbb52d30287d2d519f0586e43dda2
CHANDANJSLC <chandanjslc2001@gmail.com>
2024-05-10 19:10:51 +0530

    Changes in 8th program 4th methid

--- src/dsa/prime_number_8_3.java
+++ src/dsa/prime_number_8_3.java
@@ -2,10 +2,33 @@ package dsa;
 
 import java.util.*;
 public class prime_number_8_3 {
+	
+	
+	
+	static void prime(int n) {
+		boolean[] prime = new boolean[n+1];
+		
+		for(int i=2;i<Math.sqrt(n);i++) {
+			if(prime[i]==false) 
+			{
+				for(int j=i*i;j<=n;j=j+i)
+				{
+					prime[j]=true;
+				}
+			}
+		}
+		for(int i=2;i<=n;i++) {
+			if(prime[i]==false) {
+				System.out.println(i);
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		int n= scan.nextInt();
 		
+		prime(n);
 		
 		
 		
