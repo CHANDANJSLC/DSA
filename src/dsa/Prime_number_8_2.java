978641a978ebcaa9b73833c8d0dacd4ece2b6532
CHANDANJSLC <chandanjslc2001@gmail.com>
2024-05-10 17:27:56 +0530

    Chnages in program 8 3rd method

--- src/dsa/Prime_number_8_2.java
+++ src/dsa/Prime_number_8_2.java
@@ -8,15 +8,15 @@ public class Prime_number_8_2 {
 		if(n==1) {
 			return false;
 		}
-		if(n==2 || n==3) {
+		if(n==2 || n==3) { // 2 and 3 are prime 
 			return true;
 		}
-		if(n%2==0 || n%3==0) {
+		if(n%2==0 || n%3==0) {  //Check for other rows: if they divide by 2 and 3 is zero, then divisble by 2 and 3, then return false.
 			return false;
 		}
 		
-		for(int i=5;i< Math.sqrt(n);i=i+6) { // Start from 5
-			if(n%i==0 || n%(i+2)==0) {
+		for(int i=5;i< Math.sqrt(n);i=i+6) { // Start from 5 and go on +2and +6 is true (5+2=7), (5+6=11), these are prime i,i+2,i+6 are prime .buyt start from 5.
+			if(n%i==0 || n%(i+2)==0) {  //Same check for i and i+2 is it divisible or not 
 				return true;
 			}
 		}
