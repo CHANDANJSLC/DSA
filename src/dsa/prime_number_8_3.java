3e031c66f55e5a67bef1d170944182658a870300
CHANDANJSLC <chandanjslc2001@gmail.com>
2024-05-10 19:46:56 +0530

    Changes in program 8th

--- src/dsa/prime_number_8_3.java
+++ src/dsa/prime_number_8_3.java
@@ -6,12 +6,12 @@ public class prime_number_8_3 {
 	
 	
 	static void prime(int n) {
-		boolean[] prime = new boolean[n+1];
+		boolean[] prime = new boolean[n+1]; //Array of numbers
 		
-		for(int i=2;i<Math.sqrt(n);i++) {
-			if(prime[i]==false) 
+		for(int i=2;i<Math.sqrt(n);i++) { //Check for half of numbers. 1st factor will be in half of the numbers itself.
+			if(prime[i]==false)   //Unmarked == false 
 			{
-				for(int j=i*i;j<=n;j=j+i)
+				for(int j=i*i;j<=n;j=j+i) //Find multiplies of the number whihc is unmarked and make it as marked as true.
 				{
 					prime[j]=true;
 				}
@@ -19,7 +19,7 @@ public class prime_number_8_3 {
 		}
 		for(int i=2;i<=n;i++) {
 			if(prime[i]==false) {
-				System.out.println(i);
+				System.out.println(i);  //Print all those which are not marked that means false..
 			}
 		}
 	}
