a2f5cc62a09bb6f431463af5befe26e8b0fe62e6
CHANDANJSLC <chandanjslc2001@gmail.com>
2024-05-10 18:52:53 +0530

    Program 8 method 4

---
+++ src/dsa/prime_number_8_3.java
@@ -0,0 +1,18 @@
+package dsa;
+
+import java.util.*;
+public class prime_number_8_3 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		
+		
+		
+		
+		scan.close();
+		
+
+	}
+
+}
