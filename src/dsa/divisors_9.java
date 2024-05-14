c66eb3869222069419c163309bf4d8172b5cf299
CHANDANJSLC <chandanjslc2001@gmail.com>
2024-05-14 21:22:11 +0530

    9th program changed

--- src/dsa/divisors_9.java
+++ src/dsa/divisors_9.java
@@ -5,15 +5,11 @@ public class divisors_9 {
 	public static void main(String args[]) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		int i=0;
-		for(i =1;i<n;i++) 
-		{
+		for(int i=1; i<=n ;i++)  
+		{	
 			if(n%i==0)
 			{
-				System.out.println(i);
-				if (i != n / i) {
-                    System.out.println(n / i); 
-                }
+				System.out.println(i);			
 			}
 		}
 		scan.close();
