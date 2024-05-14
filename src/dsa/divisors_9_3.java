15d533f81656fe5fcc85ec1fa96e117261eadcb6
CHANDANJSLC <chandanjslc2001@gmail.com>
2024-05-14 21:38:04 +0530

    9th prigram 3rd metbod added

---
+++ src/dsa/divisors_9_3.java
@@ -0,0 +1,28 @@
+package dsa;
+import java.util.*;
+
+public class divisors_9_3 {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();  //40
+		int i=0;
+		for(i=1; i<=Math.sqrt(n) ;i++)  
+		{	
+			if(n%i==0) 
+			{
+				System.out.println(i);	// 1 2 4 8
+				
+			}
+		}
+			for(; i>=1 ;i--)  //Ascending order.
+			{
+				if(n%i==0)
+				{
+					System.out.println(n/i);  //8 10 20 40
+					
+				}
+			}
+
+		}
+		
+}
